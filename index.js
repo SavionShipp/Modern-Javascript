f90522d09dda2d9adf98b1db313daad57144b5cc
SavionShipp <shippsavion209@yahoo.com>
2024-09-16 17:46:13 -0700

    added webpack

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+
+var moment = require('moment'); // require
+import { sqrt } from 'mathjs'
+
 console.log('hello')
 
-var datetime = moment().startOf("day").fromNow();
-console.log(datetime);
+// var datetime = moment().startOf("day").fromNow();
+// console.log(datetime);
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+console.log(sqrt(-4).toString()) // 2i
